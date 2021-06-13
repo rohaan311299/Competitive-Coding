8a29a325def6944e0ff7364e4bffeae42a6445a3
Rohan Kacheria <rohankacheriam@gmail.com>
2021-06-13 17:14:35 +0530

    Division

---
+++ division.py
@@ -0,0 +1,12 @@
+'''
+The provided code stub reads two integers, a and b from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division, a // b. The second line should contain the result of float division, a / b.
+
+No rounding or formatting is necessary.
+'''
+a=int(input())
+b=int(input())
+print(int(a//b))
+z=float(a/b)
+print(z)
