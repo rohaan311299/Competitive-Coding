0bdc03d2752c59803d88316759a665d69986ad76
Rohan Kacheria <rohankacheriam@gmail.com>
2021-06-17 23:59:00 +0530

    Second Highest Salary

---
+++ SQL/second_highest_salary.sql
@@ -0,0 +1,7 @@
+# Leetcode
+
+# Problem Statement:- https://leetcode.com/problems/second-highest-salary/
+
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary) FROM Employee)
