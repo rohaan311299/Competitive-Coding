5209ad731ab4ae7d96a9fc5f8b235390bb8dede3
Rohan Kacheria <rohankacheriam@gmail.com>
2021-06-17 23:53:59 +0530

    SQL / PYTHON

---
+++ SQL/join_two_tables.sql
@@ -0,0 +1,7 @@
+# leetcode 
+
+# Problem Statement :- https://leetcode.com/problems/combine-two-tables/
+
+SELECT FirstName, LastName, City, State
+FROM Person
+LEFT JOIN Address ON Person.PersonId=Address.PersonId;
